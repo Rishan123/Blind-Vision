c6d98e199c8bbb95bdfa5075bd669ab86e5dd5f1
Rishan123 <alok76in@gmail.com>
2019-10-29 19:40:23 +0000

    updated classify_picamera to use buttn

--- AI_and_ML/classify_picamera.py
+++ AI_and_ML/classify_picamera.py
@@ -7,36 +7,42 @@ import numpy as np
 import PIL.Image as Image
 import matplotlib.pylab as plt
 from picamera import PiCamera
+import RPi.GPIO as GPIO
 
 camera = PiCamera()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.IN)
 
 # Download the model from :https://tfhub.dev/google/imagenet/inception_resnet_v2/classification/4
 classifier_url = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/4"
-
-camera.capture('/home/pi/cap.jpg')
-
-# Resize the image
 shape = (224, 224)
-img = Image.open(img).resize(shape)
-
-# Convert the image to a NumPy array.
-img = np.array(img)/255.0
-
 # Get the labels, and turn it into a NumPy array.
-labels_path = tf.keras.utils.get_file('ImageNetLabels.txt','https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
+labels_path = '/home/pi/tf/labels.txt'
 labels = np.array(open(labels_path).read().splitlines())
 
 # Make the Sequential model
 model = tf.keras.Sequential([
     hub.KerasLayer(classifier_url, input_shape=shape+(3,),trainable=True) 
 ])
+print('Ready')
+while True:
+    if GPIO.input(2):
+        camera.capture('/home/pi/cap.jpg')
+        img = '/home/pi/cap.jpg'
+        # Resize the image
+        img = Image.open(img).resize(shape)
 
-prediction_array = model.predict(img[np.newaxis, ...])
-prediction = np.argmax(prediction_array[0], axis=-1) # Find the top predicted class.
+        # Convert the image to a NumPy array.
+        img = np.array(img)/255.0
 
-plt.imshow(img)
-plt.axis('off')
+        prediction_array = model.predict(img[np.newaxis, ...])
+        prediction = np.argmax(prediction_array[0], axis=-1) # Find the top predicted class.
 
-prediction = labels[prediction] # Show the prediction and image using matplotlib.
-plt.title("Prediction: " + prediction.title())
-plt.show()
+        plt.imshow(img)
+        plt.axis('off')
+
+        prediction = labels[prediction] # Show the prediction and image using matplotlib.
+        plt.title("Prediction: " + prediction.title())
+        plt.show()
+        print('Ready')
+
