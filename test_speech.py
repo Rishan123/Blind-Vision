639cdfc9afe2b5b44875d7fd8d1664b9009c724e
Rishan123 <alok76in@gmail.com>
2019-10-30 16:16:57 +0000

    predictions are vey slw on tensorflow hub

--- test_speech.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-import os
-
-text = "Hello. I am Google Text to Speech"
-language = 'en'
-sp = gTTS(text=text, lang=language, slow=False)
-sp.save('/home/pi/tf/test.mp3')
-os.system('omxplayer /home/pi/tf/test.mp3')
