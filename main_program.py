ccd68ab69d38e36a6414f045006426e87a591f9f
Rishan123 <alok76in@gmail.com>
2019-11-01 16:39:15 +0000

    hh

---
+++ main_program.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+from tensorflow.keras import layers
+
+# Helper libraries
+import numpy as np
+import PIL.Image as Image
+import matplotlib.pylab as plt
+from picamera import PiCamera
+import RPi.GPIO as GPIO
+from gtts import gTTS
+import os
+
+camera = PiCamera()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(15,GPIO.IN)
+
+classifier_url = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/2"
+shape = (224, 224)
+# Get the labels, and turn it into a NumPy array.
+labels_path = '/home/pi/tf/ImageNetLabels.txt'
+labels = np.array(open(labels_path).read().splitlines())
+
+# Make the Sequential model
+model = tf.keras.Sequential([
+    hub.KerasLayer(classifier_url, input_shape=shape+(3,)) 
+])
+print('Ready')
+language = 'en'
+while True:
+    if GPIO.input(15):
+        camera.capture('/home/pi/cap.jpg')
+        img = '/home/pi/cap.jpg'
+        # Resize the image
+        img = Image.open(img).resize(shape)
+
+        # Convert the image to a NumPy array.
+        img = np.array(img)/255.0
+
+        prediction_array = model.predict(img[np.newaxis, ...])
+        prediction = np.argmax(prediction_array[0], axis=-1) # Find the top predicted class.
+
+        plt.imshow(img)
+        plt.axis('off')
+
+        prediction = labels[prediction] # Show the prediction and image using matplotlib.
+        plt.title("Prediction: " + prediction.title())
+        plt.show()
+        text = str(prediction.title())
+        sp = gTTS(text=text, lang=language, slow=False)
+        sp.save('/home/pi/tf/prediction.mp3')
+        os.system('omxplayer /home/pi/tf/prediction.mp3')
+        print('Ready')
+
