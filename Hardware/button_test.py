1fffdbe244f6d46e5130d6de585b83f9393f8dc2
Rishan123 <alok76in@gmail.com>
2019-11-02 13:38:30 +0000

    v1.0 main program made

--- Hardware/button_test.py
+++ Hardware/button_test.py
@@ -1,15 +1,22 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
+# from time import sleep
+# button = 15
+# 
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setwarnings(False)
+# GPIO.setup(button, GPIO.IN)
+# 
+# while True:
+#     if GPIO.input(button):
+#         print("ON")
+#     else:
+#         print("OFF")
+#     sleep(0.1)
+#
+
+from gpiozero import Button
 from time import sleep
-button = 15
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(button, GPIO.IN)
-
+button = Button(15, pull_up=False)
 while True:
-    if GPIO.input(button):
-        print("ON")
-    else:
-        print("OFF")
-    sleep(0.1)
- 
+    button.wait_for_press()
+    print('Hi')
--- main_program.py
+++ main_program.py
@@ -7,19 +7,24 @@ import numpy as np
 import PIL.Image as Image
 import matplotlib.pylab as plt
 from picamera import PiCamera
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+from gpiozero import Button
 from gtts import gTTS
 import os
 
 camera = PiCamera()
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(15,GPIO.IN)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(15,GPIO.IN)
+button = Button(15,pull_up=False)
 
 classifier_url = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/2"
 shape = (224, 224)
 # Get the labels, and turn it into a NumPy array.
 labels_path = '/home/pi/tf/ImageNetLabels.txt'
 labels = np.array(open(labels_path).read().splitlines())
+
+speech_labels = '/home/pi/tf/labels.txt'
+speech_labels = np.array(open(speech_labels).read().splitlines())
 
 # Make the Sequential model
 model = tf.keras.Sequential([
@@ -28,27 +33,30 @@ model = tf.keras.Sequential([
 print('Ready')
 language = 'en'
 while True:
-    if GPIO.input(15):
-        camera.capture('/home/pi/cap.jpg')
-        img = '/home/pi/cap.jpg'
-        # Resize the image
-        img = Image.open(img).resize(shape)
+    #if GPIO.input(15):
+    button.wait_for_press()
+    camera.capture('/home/pi/cap.jpg')
+    img = '/home/pi/cap.jpg'
+    # Resize the image
+    img = Image.open(img).resize(shape)
 
-        # Convert the image to a NumPy array.
-        img = np.array(img)/255.0
+    # Convert the image to a NumPy array.
+    img = np.array(img)/255.0
 
-        prediction_array = model.predict(img[np.newaxis, ...])
-        prediction = np.argmax(prediction_array[0], axis=-1) # Find the top predicted class.
+    prediction_array = model.predict(img[np.newaxis, ...])
+    prediction = np.argmax(prediction_array[0], axis=-1) # Find the top predicted class.
 
-        plt.imshow(img)
-        plt.axis('off')
+    plt.imshow(img)
+    plt.axis('off')
 
-        prediction = labels[prediction] # Show the prediction and image using matplotlib.
-        plt.title("Prediction: " + prediction.title())
-        plt.show()
-        text = str(prediction.title())
-        sp = gTTS(text=text, lang=language, slow=False)
-        sp.save('/home/pi/tf/prediction.mp3')
-        os.system('omxplayer /home/pi/tf/prediction.mp3')
-        print('Ready')
+    #prediction = labels[prediction] # Show the prediction and image using matplotlib.
+    speech = speech_labels[prediction]
+    plt.title("Prediction: " + speech.title())
+    plt.show()
+    text = str(speech.title())
+    sp = gTTS(text=text, lang=language, slow=False)
+    sp.save('/home/pi/tf/prediction.mp3')
+    os.system('omxplayer /home/pi/tf/prediction.mp3')
+    os.system('sudo rm /home/pi/cap.jpg')
+    print('Ready')
 
